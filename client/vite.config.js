d346ff3f73df9edada912964d3d6101ad9c7f027
Manish Kumar <manish320kumar230@gmail.com>
2025-07-27 11:00:40 +0530

    file modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy requests starting with /api to your backend server
+      '/api': {
+        target: 'https://image-grabber-beta.vercel.app',
+        changeOrigin: true,
+        secure: true, // set to true if using https with valid cert
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
